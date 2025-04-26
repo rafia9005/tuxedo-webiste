a14248b9ff0955f42c07f926ab304c82052267ca
Ahmad Rafi <70046808+rafia9005@users.noreply.github.com>
2025-04-26 13:18:05 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Deploy Test Jenkins #2</h1>
+      <h1>Deploy Test Jenkins Tuxedo</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
